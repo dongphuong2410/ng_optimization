f9eabcac2d7c2dd156299ca64552ad35ec542e6c
Phuong Do <dongphuong2004@gmail.com>
2019-07-13 23:34:42 +0700

    Init project with maven

---
+++ src/main/java/com/Location.java
@@ -0,0 +1,36 @@
+public class Location {
+    private int x;
+    private int y;
+    private String code;
+
+    public Location(int x, int y, String code) {
+        this.x = x;
+        this.y = y;
+        this.code = code;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+}
