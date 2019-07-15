fae61277591b9887c56d128209b038b55f4b62b3
Phuong Do <dongphuong2004@gmail.com>
2019-07-15 09:45:09 +0700

    Calculate distance between two label set

--- src/main/java/com/label/LabelSet.java
+++ src/main/java/com/label/LabelSet.java
@@ -1,25 +1,45 @@
 package main.java.com.label;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 import main.java.com.label.Label;
 import main.java.com.label.InclusiveLabel;
 import main.java.com.label.ExclusiveLabel;
+import main.java.com.Constants;
 
 public class LabelSet {
-    private HashSet<InclusiveLabel> inSet;
-    private HashSet<ExclusiveLabel> exSet;
+    private HashSet<Label> labels;
 
     public LabelSet() {
-        inSet = new HashSet<InclusiveLabel>();
-        exSet = new HashSet<ExclusiveLabel>();
+        labels = new HashSet<Label>();
     }
 
-    public void addLabel(InclusiveLabel label) {
-        inSet.add(label);
+    public void addLabel(Label label) {
+        labels.add(label);
     }
 
-    public void addLabel(ExclusiveLabel label) {
-        exSet.add(label);
+    public int distanceTo(LabelSet target) {
+        int sum = 0;
+        int count = 0;
+        Iterator<Label> i = labels.iterator();
+        Iterator<Label> j = target.labels.iterator();
+        if (!i.hasNext() || !j.hasNext()) {
+            return Constants.NORMAL_DISTANCE;
+        }
+        while (i.hasNext()) {
+            Label lbl_one = i.next();
+            while (j.hasNext()) {
+                Label lbl_two = j.next();
+                int dist = lbl_one.distanceTo(lbl_two);
+                if (dist == 0)
+                    return Constants.NORMAL_DISTANCE;
+                else {
+                    sum += dist;
+                    count++;
+                }
+            }
+        }
+        return sum / count;
     }
 }
